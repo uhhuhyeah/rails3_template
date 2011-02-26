a4eec44679832c039f0daf860e3ff011ee0938ac
David A McClain <david@uhhuhyeah.com>
2011-02-26 11:06:16 -0800

    update to curl

--- main.rb
+++ main.rb
@@ -7,33 +7,43 @@ run "rm public/javascripts/rails.js"
 run "rm app/views/layouts/application.html.erb"
 
 # Grab standards gems
-run "wget --no-check-certificate '#{@git_repo}/raw/master/Gemfile' -O Gemfile"
+run "curl -O #{@git_repo}/raw/master/Gemfile"
 run "bundle install"
 
 # Load settings
-run "wget --no-check-certificate '#{@git_repo}/raw/master/load_settings.rb' -O config/load_settings.rb"
-run "wget --no-check-certificate '#{@git_repo}/raw/master/settings.yml' -O config/settings.yml"
+inside('config') do
+  run "curl -O #{@git_repo}/raw/master/load_settings.rb"
+  run "curl -O #{@git_repo}/raw/master/settings.yml"
+end
 
 # Run generators
 run "rails generate jquery:install"
 run "rails generate rspec:install"
 run "rails generate cucumber:install"
 run "rails generate devise:install"
-run "rails generate devise User" if?('Generate Devise User?')
+run "rails generate devise User" if yes?("Generate Devise User?")
 
 # Setup Sass
-run "mkdir public/sass"
-run "wget --no-check-certificate '#{@git_repo}/raw/master/screen.sass' -O public/stylesheets/sass/screen.sass"
-run "wget --no-check-certificate '#{@git_repo}/raw/master/_mixins.sass' -O public/stylesheets/sass/_mixins.sass"
+run "mkdir -p public/stylesheets/sass"
+inside('public/stylesheets/sass') do
+  run "curl -O #{@git_repo}/raw/master/screen.sass"
+  run "curl -O #{@git_repo}/raw/master/_mixins.sass"
+end
 
 # Download head.js and application layout
-run "wget --no-check-certificate '#{@git_repo}/raw/master/head.min.js' -O public/javascripts/head.min.js"
-run "wget --no-check-certificate '#{@git_repo}/raw/master/application.html.erb' -O app/views/layouts/application.html.erb"
+inside('public/javascripts') do
+  run "curl -O #{@git_repo}/raw/master/head.min.js"
+  run "curl -O #{@git_repo}/raw/master/application.html.erb"
+end
 
+# Setup home page
+generate :controller, "welcome index" 
+route "root :to => 'welcome#index'"
 
 # git
 git :init
 git :add => "."
 git :commit => "-am 'Initial Commit'"
 
-puts "Done. Now go make it rain!"
+puts "Done.\n"
+puts "Now go make it rain!"
